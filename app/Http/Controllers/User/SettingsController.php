98d2cf53f69dc585a61bdec2f85deb9891897613
Matovu Ronald <maronwhite6@gmail.com>
2020-03-22 20:10:47 +0300

    Update User Password

--- app/Http/Controllers/User/SettingsController.php
+++ app/Http/Controllers/User/SettingsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
+use App\Rules\CheckSamePassword;
+use App\Rules\MatchOldPassword;
 use Grimzy\LaravelMysqlSpatial\Types\Point;
 
 class SettingsController extends Controller
@@ -36,4 +38,17 @@ class SettingsController extends Controller
         return new UserResource($user);
     }
 
+    public function updatePassword(Request $request)
+    {
+        $this->validate($request, [
+            'current_password' => ['required', new MatchOldPassword],
+            'password' => ['required', 'confirmed', 'min:6', new CheckSamePassword]
+        ]);
+
+        $request->user()->update([
+            'password' => bcrypt($request->password)
+        ]);
+
+        return response()->json(["message" => "Password Updated Successfully"]);
+    }
 }

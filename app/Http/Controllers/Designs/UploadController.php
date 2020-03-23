4a39ff709fa97f09e3a6c537bf9ab0601ad62180
Matovu Ronald <maronwhite6@gmail.com>
2020-03-23 20:13:15 +0300

    Create UploadController.php

---
+++ app/Http/Controllers/Designs/UploadController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Designs;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UploadController extends Controller
+{
+    public function upload(Request $request)
+    {
+    }
+}

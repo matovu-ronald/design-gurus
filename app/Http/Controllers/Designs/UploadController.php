b0ba0ae5a63134eb7258026aa9c4cb54d5a783a3
Matovu Ronald <maronwhite6@gmail.com>
2020-03-24 08:08:59 +0300

    Upload image to tmp local disk

--- app/Http/Controllers/Designs/UploadController.php
+++ app/Http/Controllers/Designs/UploadController.php
@@ -2,15 +2,38 @@
 
 namespace App\Http\Controllers\Designs;
 
-use App\Http\Controllers\Controller;
+use App\Jobs\UploadImage;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class UploadController extends Controller
 {
     public function upload(Request $request)
     {
         $this->validate($request, [
-            'image' => ['required', 'mime:jpeg,gif,bmp,png,jpg', 'max:2048']
+            'image' => ['required', 'mimes:jpeg,gif,bmp,png,jpg', 'max:2048']
         ]);
+
+        // Get the image
+        $image = $request->file('image');
+        $image_path = $image->getPathname();
+
+        // Get the original file name and replace spaces with underscore
+        // Business Cards.png = timestamp()_business_card.png
+        $filename = time() . "_" . preg_replace('/\s+/', '_', strtolower($image->getClientOriginalName()));
+
+        // Move the image to the temporary location (temp)
+        $tmp = $image->storeAs('uploads/original', $filename, 'tmp');
+
+        // Create the database record for the design
+        $design = auth()->user()->designs()->create([
+            'image' => $filename,
+            'disk' => config('site.upload_disk')
+        ]);
+
+        // Dispatch the job to handle image manipulation.
+        $this->dispatch(new UploadImage($design));
+
+        return response()->json($design, 200);
     }
 }

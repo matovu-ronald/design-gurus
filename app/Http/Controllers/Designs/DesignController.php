dfb9fff22b72b319dbf082f67213b0b41d6a8433
Matovu Ronald <ronald.matovu@tollandhaben.com>
2020-03-25 19:07:06 +0300

    Add authorization policy to updating a design

--- app/Http/Controllers/Designs/DesignController.php
+++ app/Http/Controllers/Designs/DesignController.php
@@ -6,18 +6,21 @@ use App\Models\Design;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\DesignResource;
 
 class DesignController extends Controller
 {
     public function update(Request $request, $id)
     {
+        $design = Design::find($id);
+
+        $this->authorize('update', $design);
+
         $this->validate($request, [
             'title' => ['required', 'max:255', 'unique:designs,title,' . $id],
             'description' => ['required', 'min:20', 'max:200', 'string'],
             'is_live' => ['required']
         ]);
-
-        $design = Design::find($id);
 
         $design->update([
             'title' => $request->title,

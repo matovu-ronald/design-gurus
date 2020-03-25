2b5741a672e2246bfca78495e271ce30c859572f
Matovu Ronald <maronwhite6@gmail.com>
2020-03-25 20:23:32 +0300

    Create DeleteImage.php

---
+++ app/Jobs/DeleteImage.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Design;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class DeleteImage implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $disk;
+    protected $image;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($disk, $image)
+    {
+        $this->disk = $disk;
+        $this->image = $image;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // Delete the files associated with the record.
+        foreach(['thumbnail', 'large', 'original'] as $size) {
+            // Check if the file exists on the disk
+            if (Storage::disk($this->disk)->exists("uploads/designs/{$size}/". $this->image)) {
+                Storage::disk($this->disk)->delete("uploads/designs/{$size}/". $this->image);
+            }
+        }
+
+    }
+}

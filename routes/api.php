3439bc3e8d77d2845f93b1e34a45457a2861f45f
Matovu Ronald <maronwhite6@gmail.com>
2020-03-25 20:24:02 +0300

    Delete design route

--- routes/api.php
+++ routes/api.php
@@ -36,4 +36,5 @@ Route::group(['middleware' => ['auth:api']], function () {
     // Upload Designs
     Route::post('designs', 'Designs\UploadController@upload');
     Route::put('designs/{id}', 'Designs\DesignController@update');
+    Route::delete('designs/{design}', 'Designs\DesignController@destroy');
 });

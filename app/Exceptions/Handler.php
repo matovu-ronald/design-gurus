59d46bc07cbf4af7420558129d1b7a80ae9fc822
Matovu Ronald <maronwhite6@gmail.com>
2020-03-25 20:22:57 +0300

    Return json response for ModelNotFoundException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Throwable;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -58,6 +59,15 @@ class Handler extends ExceptionHandler
                 ]], 403);
             }
         }
+
+        if ($exception instanceof ModelNotFoundException && $request->expectsJson()) {
+            return response()->json([
+                "errors" => [
+                    "message" => "The resource was not found in the database"
+                ]
+            ], 404);
+        }
+
         return parent::render($request, $exception);
     }
 }

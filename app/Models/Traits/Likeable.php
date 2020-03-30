b66cb15512fca097683d47866ec702457066099a
Matovu Ronald <ronald.matovu@tollandhaben.com>
2020-03-30 15:42:04 +0300

    Add like and unlike functionality to the Likeable trait

--- app/Models/Traits/Likeable.php
+++ app/Models/Traits/Likeable.php
@@ -10,4 +10,49 @@ trait Likeable
     {
         return $this->morphMany(Like::class, 'likeable');
     }
+
+    /**
+     * Like model
+     */
+    public function like()
+    {
+        if (!auth()->check()) {
+            return;
+        }
+
+        // Check if the current user had already liked the model
+        if ($this->isLikedByUser(auth()->id())) {
+            return;
+        }
+
+        // Create like in the database
+        $this->likes()->create(['user_id' => auth()->id()]);
+    }
+
+    /**
+     * Unlike model
+     */
+    public function unlike()
+    {
+        if (!auth()->check()) {
+            return;
+        }
+
+        // Check if the current user had already liked the model
+        if (!$this->isLikedByUser(auth()->id())) {
+            return;
+        }
+
+        $this->likes()
+            ->where('user_id', auth()->id())
+            ->delete();
+    }
+
+    public function isLikedByUser($userId)
+    {
+        return (bool)$this->likes()
+            ->where('user_id', $userId)
+            ->count();
+    }
+
 }

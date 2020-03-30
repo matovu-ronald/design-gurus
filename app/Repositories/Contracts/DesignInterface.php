1b4f52c0755b6a33388d388f5f7370544a9351ee
Matovu Ronald <ronald.matovu@tollandhaben.com>
2020-03-30 15:39:53 +0300

    Add like($id) and isLikedByUser($designId) method signatures

--- app/Repositories/Contracts/DesignInterface.php
+++ app/Repositories/Contracts/DesignInterface.php
@@ -7,4 +7,8 @@ interface DesignInterface
     public function applyTags($id, array $data);
 
     public function addComment($designId, array $data);
+
+    public function like($id);
+
+    public function isLikedByUser($designId);
 }

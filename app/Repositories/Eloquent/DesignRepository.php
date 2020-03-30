7c614a957fa952f588c38a2c71707f9a5fdbbd1b
Matovu Ronald <maronwhite6@gmail.com>
2020-03-30 10:28:04 +0300

    Implement addComment($designId, array $data)

--- app/Repositories/Eloquent/DesignRepository.php
+++ app/Repositories/Eloquent/DesignRepository.php
@@ -17,4 +17,15 @@ class DesignRepository extends BaseRepository implements DesignInterface
         $design = $this->find($id);
         $design->retag($data);
     }
+
+    public function addComment($designId, array $data)
+    {
+        // Get the design for which we want to create a comment.
+        $design = $this->find($designId);
+
+        // Create the comment for the design
+        $comment = $design->comments()->create($data);
+
+        return $comment;
+    }
 }

9273ac2ca39ec528f427c701e615e76740770dec
Matovu Ronald <ronald.matovu@tollandhaben.com>
2020-03-30 10:30:16 +0300

    Inject CommentInterface and DesignInterface in plus add  store, update, destroy methods to the controller

--- app/Http/Controllers/Designs/CommentController.php
+++ app/Http/Controllers/Designs/CommentController.php
@@ -2,10 +2,62 @@
 
 namespace App\Http\Controllers\Designs;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CommentResource;
+use App\Repositories\Contracts\CommentInterface;
+use App\Repositories\Contracts\DesignInterface;
 
 class CommentController extends Controller
 {
-    //
+    protected $comments;
+    protected $designs;
+
+    public function __construct(CommentInterface $comments, DesignInterface $designs)
+    {
+        $this->comments = $comments;
+        $this->designs = $designs;
+    }
+
+    public function store(Request $request, $designId)
+    {
+        $this->validate($request, [
+            'body' => ['required']
+        ]);
+
+        $comment = $this->designs->addComment($designId, [
+            'body' => $request->body,
+            'user_id' => auth()->id()
+        ]);
+
+        return new CommentResource($comment);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $comment = $this->comments->find($id);
+
+        $this->authorize('update', $comment);
+
+        $this->validate($request, [
+            'body' => ['required']
+        ]);
+
+        $comment = $this->comments->update($id, [
+            'body' => $request->body
+        ]);
+
+        return new CommentResource($comment);
+    }
+
+    public function destroy($id)
+    {
+        $comment = $this->comments->find($id);
+
+        $this->authorize('delete', $comment);
+
+        $this->comments->delete($id);
+
+        return response()->json(['message' => 'Comment deleted successfully'], 200);
+    }
 }

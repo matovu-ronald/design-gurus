2d66f383ea409f26343b2928cdffd14fc18cbf30
Matovu Ronald <maronwhite6@gmail.com>
2020-03-30 07:33:34 +0000

    Apply fixes from StyleCI

--- app/Http/Resources/DesignResource.php
+++ app/Http/Resources/DesignResource.php
@@ -34,7 +34,7 @@ class DesignResource extends JsonResource
                 'updated_at_human' => $this->updated_at->diffForHumans(),
                 'updated_at' => $this->updated_at,
             ],
-            'comments' => CommentResource::collection($this->comments)
+            'comments' => CommentResource::collection($this->comments),
         ];
     }
 }

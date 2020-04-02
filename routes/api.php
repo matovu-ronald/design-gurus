5f0a26a09dc0ecee01be1f34b5f5b073a87abb66
Matovu Ronald <maronwhite6@gmail.com>
2020-04-02 14:52:16 +0300

    Add teams routes

--- routes/api.php
+++ routes/api.php
@@ -13,17 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Public routes
+/**
+ * Public routes
+ *
+ */
+// Me
 Route::get('me', 'User\MeController@getMe');
 
-// Design Routes
+// Design
 Route::get('designs', 'Designs\DesignController@index');
 Route::get('designs/{id}', 'Designs\DesignController@findDesign');
 
-// Users Routes
+// Users
 Route::get('users', 'User\UserController@index');
 
-// Guest routes
+// Teams
+Route::get('teams/slug/{slug}', 'Teams\TeamController@findBySlug');
+
+/**
+ * Guest routes
+ *
+ */
 Route::group(['middleware' => ['guest:api']], function () {
     Route::post('register', 'Auth\RegisterController@register');
     Route::post('login', 'Auth\LoginController@login');
@@ -33,7 +43,10 @@ Route::group(['middleware' => ['guest:api']], function () {
     Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 });
 
-// Authenticated routes
+/**
+ * Authenticated routes
+ *
+ */
 Route::group(['middleware' => ['auth:api']], function () {
     // User settings
     Route::post('logout', 'Auth\LoginController@logout');
@@ -53,4 +66,12 @@ Route::group(['middleware' => ['auth:api']], function () {
     // Likes and Unlikes
     Route::post('designs/{id}/like', 'Designs\DesignController@like');
     Route::post('designs/{designId}/liked', 'Designs\DesignController@checkIfUserHasLiked');
+
+    // Teams
+    Route::post('teams', 'Teams\TeamController@store');
+    Route::get('teams/{id}', 'Teams\TeamController@findById');
+    Route::get('teams', 'Teams\TeamController@index');
+    Route::get('users/teams', 'Teams\TeamController@fetchUserTeams');
+    Route::put('teams/{id}', 'Teams\TeamController@update');
+    Route::delete('teams/{id}', 'Teams\TeamController@destroy');
 });

986b1244d10272951555f38236187462a6389b33
Matovu Ronald <maronwhite6@gmail.com>
2020-06-20 16:28:45 +0300

    Add belongsTo relationship

--- app/Models/Design.php
+++ app/Models/Design.php
@@ -13,6 +13,7 @@ class Design extends Model
 
     protected $fillable = [
         'user_id',
+        'team_id',
         'image',
         'title',
         'description',
@@ -26,6 +27,11 @@ class Design extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
     }
 
     public function comments()

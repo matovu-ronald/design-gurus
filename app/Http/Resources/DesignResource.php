b985df11f1666fafe599960a7a12a3fd9a92802a
Matovu Ronald <maronwhite6@gmail.com>
2020-06-20 16:32:01 +0300

    Return team in on a design

--- app/Http/Resources/DesignResource.php
+++ app/Http/Resources/DesignResource.php
@@ -35,6 +35,10 @@ class DesignResource extends JsonResource
                 'updated_at_human' => $this->updated_at->diffForHumans(),
                 'updated_at' => $this->updated_at,
             ],
+            'team' => $this->team ? [
+                'name' => $this->team->name,
+                'slug' => $this->team->slug
+            ] : null,
             'comments' => CommentResource::collection($this->whenLoaded('comments')),
         ];
     }
